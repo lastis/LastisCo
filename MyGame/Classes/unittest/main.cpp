7870484b9efce8453ae78e355b7c19f4e6a292b1
lastis <neitakkikkebanan@gmail.com>
2015-03-05 17:32:05 +0100

    Seems the program inside the cocos framework.

--- MyGame/Classes/unittest/main.cpp
+++ MyGame/Classes/unittest/main.cpp
@@ -1,13 +1,15 @@
 #include <unittest++/UnitTest++.h>
 #include "../core/ShipMap.h"
-
-
+#include <iostream>
 
 SUITE(Some){
+    ShipMap map;
+
     TEST(Instantiate){
-        double hey = 0;
+        map = ShipMap(2,2);
     }
 }
+
 int main(int argc, const char *argv[])
 {
 	return UnitTest::RunAllTests();
