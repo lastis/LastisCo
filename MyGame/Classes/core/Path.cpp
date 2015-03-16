a47c8ba4f6be383c4b1b5c23053a2d73c8f923f9
lastis <neitakkikkebanan@gmail.com>
2015-03-16 21:07:16 +0100

    "Finished" the small path class. Not tested yet.

--- MyGame/Classes/core/Path.cpp
+++ MyGame/Classes/core/Path.cpp
@@ -1,6 +1,28 @@
+#ifdef TESTING
+#include <iostream>
+#endif
+
 #include "Path.h"
 Path::Path(unsigned int* path, int length){
     this->path = path;
     this->length = length;
     index = 0;
+}
+
+bool Path::isComplete(){
+    if (index == length) return true;
+    else false;
+}
+
+bool Path::hasPath(){
+    if (path != NULL) return true;
+    else return false;
+}
+
+unsigned int Path::getNextDirection(){
+    using namespace directions;
+    if (!hasPath()) return NO_DIRECTION;
+    if (isComplete()) return NO_DIRECTION;
+    index++;
+    return path[index-1];
 }
--- MyGame/Classes/core/Path.h
+++ MyGame/Classes/core/Path.h
@@ -1,11 +1,16 @@
 #ifndef __PATH_H_INCLUDED__
 #define __PATH_H_INCLUDED__
+#include "Identifiers.h"
+
 class Path {
 public:
     unsigned int* path;
     int length;
     int index;
     Path(unsigned int* path, int length);
+    unsigned int getNextDirection();
+    bool isComplete();
+    bool hasPath();
 
 };
 
