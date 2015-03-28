2e57488e4aaed0fca784ada146ba61aedfe0f61a
lastis <neitakkikkebanan@gmail.com>
2015-03-28 23:14:12 +0100

    Added a base class for all objects which they should inherit from.

--- MyGame/Classes/core/objects/Corn.h
+++ MyGame/Classes/core/objects/Corn.h
@@ -1,9 +1,9 @@
 #ifndef __CORN_H_INCLUDED__
 #define __CORN_H_INCLUDED__
-#include "../Base.h"
+#include "Object.h"
 
 class Person;
-class Corn : public Base{
+class Corn : public Object{
 public:
     Corn();
     void update();
---
+++ MyGame/Classes/core/objects/Object.h
@@ -0,0 +1,12 @@
+#ifndef __OBJECT_H_INCLUDED__
+#define __OBJECT_H_INCLUDED__
+
+#include "../Base.h"
+// Base class for all objects. Used to be able to make an 
+// array of objects.
+class Object : public Base{
+public:
+    Object();
+
+};
+#endif
